1057d386da9de7dda73a5d4df7ec1c94ce513098
Thomas Bocandé <thomasbocande@gmail.com>
2024-11-30 21:37:31 +0100

    got boundaries now

--- PathDrawer.py
+++ PathDrawer.py
@@ -1,47 +1,14 @@
 import cv2
 import numpy as np
-from scipy.interpolate import splprep, splev
-
-def set_background_with_texture(canvas, texture):
-        canvas_h, canvas_w = canvas.shape[:2]
-        texture_h, texture_w = texture.shape[:2]
-
-        # Tile the texture
-        for y in range(0, canvas_h, texture_h):
-            for x in range(0, canvas_w, texture_w):
-                # Determine the region of interest (ROI) on the canvas
-                x_end = min(x + texture_w, canvas_w)
-                y_end = min(y + texture_h, canvas_h)
-
-                # Determine the corresponding ROI from the texture
-                texture_roi = texture[: y_end - y, : x_end - x]
-
-                canvas[y:y_end, x:x_end] = texture_roi
-
-        return canvas
-    
-def calculate_spline(points):
-    k = min(len(points) - 1, 5)
-    points = np.array(points)
-    tck, u = splprep([points[:, 0], points[:, 1]], s=0, k=k)
-    u_fine = np.linspace(0, 1, 500)
-    x_new, y_new = splev(u_fine, tck)
-    return x_new, y_new  
-    
-def draw_spline(image, points, color):
-        x_new, y_new = calculate_spline(points)
-        for i in range(len(x_new) - 1):
-            pt1 = (int(x_new[i]), int(y_new[i]))
-            pt2 = (int(x_new[i + 1]), int(y_new[i + 1]))
-            cv2.line(image, pt1, pt2, color, 2)
-
+from utils import *
 class PathDrawer:
     def __init__(
         self, path_texture, grass_texture, 
-        height=600, width=800, grid_size=(64, 64), 
+        height=600, width=800, grid_size=(64, 64), path_width=100,
         points_color=(255, 255, 255), line_color=(255, 0, 255)
     ):
         self.selected_points = []
+        self.path_width = path_width
         
         self.points_color = points_color
         self.line_color = line_color
@@ -68,10 +35,20 @@ class PathDrawer:
     
     def draw_path(self):
         if len(self.selected_points) == 2:
-            pt1, pt2 = self.selected_points
-            cv2.line(self.image, pt1, pt2, self.line_color, 2)
+            draw_line_with_width(
+                self.image, 
+                self.selected_points, 
+                self.line_color,
+                self.path_width
+            )
+            self.spline_drawn = True
         elif len(self.selected_points) > 2:
-            draw_spline(self.image, self.selected_points, self.line_color)
+            draw_spline_with_width(
+                self.image, 
+                self.selected_points, 
+                self.line_color,
+                self.path_width
+            )
             self.spline_drawn = True
         else:
             print("Need at least 2 points to draw a path")
@@ -79,27 +56,18 @@ class PathDrawer:
     def toggle_spline(self):
         if self.spline_drawn:
             self.spline_drawn = False
-            self.reset_image()  # Reset the image if the spline is being hidden
+            self.reset_image()
             for point in self.selected_points:
                 cv2.circle(self.image, point, 5, self.points_color, -1)
             cv2.imshow("Path from Points", self.image)
         else:
-            self.draw_path()  # Redraw the spline if it was hidden
+            self.draw_path()
             cv2.imshow("Path from Points", self.image)
     
     def draw_path_with_texture(self):
         self.reset_image()
         if len(self.selected_points) == 2:
-            x_new = np.linspace(
-                self.selected_points[0][0], 
-                self.selected_points[1][0], 
-                100
-            )
-            y_new = np.linspace(
-                self.selected_points[0][1], 
-                self.selected_points[1][1], 
-                100
-            )
+            x_new, y_new = calculate_lerp_points(self.selected_points[0], self.selected_points[1])
         elif len(self.selected_points) > 2:
             x_new, y_new = calculate_spline(self.selected_points)
         else:
@@ -107,7 +75,6 @@ class PathDrawer:
             return
     
         grid_width, grid_height = self.grid_size
-
         resized_texture = cv2.resize(self.path_texture, self.grid_size)
 
         # Determine which grid cells the path overlaps
@@ -115,17 +82,14 @@ class PathDrawer:
             grid_x = int(x // grid_width)
             grid_y = int(y // grid_height)
 
-            # Calculate the top-left corner of the grid cell
             top_left_x = grid_x * grid_width
             top_left_y = grid_y * grid_height
 
-            # Clip texture to fit within canvas boundaries
             x_start = max(0, top_left_x)
             y_start = max(0, top_left_y)
             x_end = min(self.image.shape[1], x_start + grid_width)
             y_end = min(self.image.shape[0], y_start + grid_height)
 
-            # Compute the region of interest (ROI) on the texture
             texture_roi = resized_texture[: y_end - y_start, : x_end - x_start]
 
             self.image[y_start:y_end, x_start:x_end] = texture_roi
---
+++ utils.py
@@ -0,0 +1,93 @@
+import numpy as np
+from scipy.interpolate import splprep, splev
+import cv2
+
+def set_background_with_texture(canvas, texture):
+        canvas_h, canvas_w = canvas.shape[:2]
+        texture_h, texture_w = texture.shape[:2]
+
+        for y in range(0, canvas_h, texture_h):
+            for x in range(0, canvas_w, texture_w):
+                x_end = min(x + texture_w, canvas_w)
+                y_end = min(y + texture_h, canvas_h)
+
+                texture_roi = texture[: y_end - y, : x_end - x]
+
+                canvas[y:y_end, x:x_end] = texture_roi
+
+        return canvas
+    
+def calculate_spline(points):
+    k = min(len(points) - 1, 5)
+    points = np.array(points)
+    tck, u = splprep([points[:, 0], points[:, 1]], s=0, k=k)
+    u_fine = np.linspace(0, 1, 500)
+    x_new, y_new = splev(u_fine, tck)
+    return x_new, y_new
+
+def calculate_lerp_points(pt1, pt2, num_points=100):
+    x_new = np.linspace(pt1[0], pt2[0], num_points)
+    y_new = np.linspace(pt1[1], pt2[1], num_points)
+    return x_new, y_new
+
+def calculate_spline_with_tangents(points):
+    k = min(len(points) - 1, 5)
+    points = np.array(points)
+    tck, u = splprep([points[:, 0], points[:, 1]], s=0, k=k)
+    u_fine = np.linspace(0, 1, 500)
+    x_new, y_new = splev(u_fine, tck)
+    
+    dx, dy = splev(u_fine, tck, der=1)
+    tangents = np.array([dx, dy]).T
+    tangents /= np.linalg.norm(tangents, axis=1)[:, None]  # Normalize tangents
+    
+    return x_new, y_new, tangents
+
+def draw_line(image, points, color):
+    pt1, pt2 = points
+    cv2.line(image, pt1, pt2, color, 2)
+    
+def draw_line_with_width(image, points, color, width):
+    half_width = width // 2
+    pt1, pt2 = points
+    cv2.line(image, pt1, pt2, color, 2)
+    
+    pt1_left = (pt1[0] - half_width, pt1[1])
+    pt1_right = (pt1[0] + half_width, pt1[1])
+    pt2_left = (pt2[0] - half_width, pt2[1])
+    pt2_right = (pt2[0] + half_width, pt2[1])
+    
+    cv2.line(image, pt1_left, pt2_left, (255, 0, 0), 2)
+    cv2.line(image, pt1_right, pt2_right, (255, 0, 0), 2)
+    
+def draw_spline(image, points, color):
+        x_new, y_new = calculate_spline(points)
+        for i in range(len(x_new) - 1):
+            pt1 = (int(x_new[i]), int(y_new[i]))
+            pt2 = (int(x_new[i + 1]), int(y_new[i + 1]))
+            cv2.line(image, pt1, pt2, color, 2)
+
+def draw_spline_with_width(image, points, color, width):
+    half_width = width // 2
+    x_new, y_new, tangents = calculate_spline_with_tangents(points)
+
+    for i in range(len(x_new) - 1):
+        pt1 = np.array([int(x_new[i]), int(y_new[i])])
+        pt2 = np.array([int(x_new[i + 1]), int(y_new[i + 1])])
+
+        tangent = tangents[i]
+        tangent_length = np.linalg.norm(tangent)
+        if tangent_length != 0:  # Normalize the tangent
+            tangent = tangent / tangent_length
+
+        normal = np.array([-tangent[1], tangent[0]])
+
+        p1_left = (pt1 + half_width * normal).astype(int)
+        p2_left = (pt2 + half_width * normal).astype(int)
+        p1_right = (pt1 - half_width * normal).astype(int)
+        p2_right = (pt2 - half_width * normal).astype(int)
+
+        cv2.line(image, tuple(pt1), tuple(pt2), color, 2)
+
+        cv2.line(image, tuple(p1_left), tuple(p2_left), (255, 0, 0), 2)
+        cv2.line(image, tuple(p1_right), tuple(p2_right), (255, 0, 0), 2)
