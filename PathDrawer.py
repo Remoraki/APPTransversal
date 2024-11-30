784a390b2193daa55d9801d155931aa64101d687
Thomas Bocandé <thomasbocande@gmail.com>
2024-11-30 12:16:07 +0100

    QoL

--- PathDrawer.py
+++ PathDrawer.py
@@ -2,36 +2,6 @@ import cv2
 import numpy as np
 from scipy.interpolate import splprep, splev
 
-def set_background_with_texture(canvas, texture):
-    """
-    Fills the given canvas with a tiling texture.
-    
-    Args:
-        canvas (np.ndarray): The canvas where the background will be applied.
-        texture (np.ndarray): The tilable texture image.
-    
-    Returns:
-        np.ndarray: The canvas filled with the tiling texture.
-    """
-    # Get dimensions
-    canvas_h, canvas_w = canvas.shape[:2]
-    texture_h, texture_w = texture.shape[:2]
-
-    # Tile the texture
-    for y in range(0, canvas_h, texture_h):
-        for x in range(0, canvas_w, texture_w):
-            # Determine the region of interest (ROI) on the canvas
-            x_end = min(x + texture_w, canvas_w)
-            y_end = min(y + texture_h, canvas_h)
-
-            # Determine the corresponding ROI from the texture
-            texture_roi = texture[: y_end - y, : x_end - x]
-
-            # Paste the texture into the canvas
-            canvas[y:y_end, x:x_end] = texture_roi
-
-    return canvas
-
 class PathDrawer:
     def __init__(self, path_texture, grass_texture, height=600, width=800, grid_size=(64, 64), points_color=(255, 255, 255), line_color=(255, 0, 255)):
         self.selected_points = []
@@ -41,26 +11,38 @@ class PathDrawer:
         self.path_texture = path_texture
         self.grid_size = grid_size
         self.grass_texture = cv2.resize(grass_texture, (256, 256))
-        self.image = set_background_with_texture(self.image, self.grass_texture)
+        self.image = self.set_background_with_texture(self.image, self.grass_texture)
+        self.background = np.copy(self.image)
+        self.spline_drawn = False
     
     def select_points(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
             self.selected_points.append((x, y))
             cv2.circle(self.image, (x, y), 5, self.points_color, -1)
             cv2.imshow("Path from Points", self.image)
-    
-    def points_loop(self):
-        while True:
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord('d'):     # Draw path
-                break
-            if key == ord('u'):
-                if len(self.selected_points) > 0:
-                    self.selected_points.pop()
-                    self.image = np.zeros((500, 500, 3), dtype=np.uint8)
-                    for point in self.selected_points:
-                        cv2.circle(self.image, point, 5, self.points_color, -1)
-                    cv2.imshow("Path from Points", self.image)
+            
+    def reset_image(self):
+        self.image = np.copy(self.background) 
+            
+    def set_background_with_texture(self, canvas, texture):
+        # Get dimensions
+        canvas_h, canvas_w = canvas.shape[:2]
+        texture_h, texture_w = texture.shape[:2]
+
+        # Tile the texture
+        for y in range(0, canvas_h, texture_h):
+            for x in range(0, canvas_w, texture_w):
+                # Determine the region of interest (ROI) on the canvas
+                x_end = min(x + texture_w, canvas_w)
+                y_end = min(y + texture_h, canvas_h)
+
+                # Determine the corresponding ROI from the texture
+                texture_roi = texture[: y_end - y, : x_end - x]
+
+                # Paste the texture into the canvas
+                canvas[y:y_end, x:x_end] = texture_roi
+
+        return canvas
     
     def draw_spline(self):
         if len(self.selected_points) > 3:
@@ -72,11 +54,21 @@ class PathDrawer:
                 pt1 = (int(x_new[i]), int(y_new[i]))
                 pt2 = (int(x_new[i + 1]), int(y_new[i + 1]))
                 cv2.line(self.image, pt1, pt2, self.line_color, 2)
+            self.spline_drawn = True
         else:
             #TODO: add linear and cubic interpolation
             print("Need at least 4 points to draw a path")
-            self.points_loop()
-            self.draw_spline()
+    
+    def toggle_spline(self):
+        if self.spline_drawn:
+            self.spline_drawn = False
+            self.reset_image()  # Reset the image if the spline is being hidden
+            for point in self.selected_points:
+                cv2.circle(self.image, point, 5, self.points_color, -1)
+            cv2.imshow("Path from Points", self.image)
+        else:
+            self.draw_spline()  # Redraw the spline if it was hidden
+            cv2.imshow("Path from Points", self.image)
     
     def draw_path_with_texture(self):
         if len(self.selected_points) > 3:
@@ -120,5 +112,3 @@ class PathDrawer:
         else:
             #TODO: add linear and cubic interpolation
             print("Need at least 4 points to draw a path")
-            self.points_loop()
-            self.draw_path()
--- main.py
+++ main.py
@@ -7,15 +7,30 @@ LINE_COLOR = (255, 0, 255)
 texture = cv2.imread("Textures/texture3.png")
 grass_texture = cv2.imread("Textures/tilable-img_0044-dark.png")
 
-PathDrawer = PathDrawer(texture, grass_texture, points_color=POINT_COLOR, line_color=LINE_COLOR)
+path_drawer = PathDrawer(texture, grass_texture, points_color=POINT_COLOR, line_color=LINE_COLOR)
 
-cv2.imshow("Path from Points", PathDrawer.image)
-cv2.setMouseCallback("Path from Points", PathDrawer.select_points)
+cv2.imshow("Path from Points", path_drawer.image)
+cv2.setMouseCallback("Path from Points", path_drawer.select_points)
 
-PathDrawer.points_loop()
+while True:
+    key = cv2.waitKey(1) & 0xFF
+    
+    if key == ord('d'):  # Draw the path when 'd' is pressed
+        path_drawer.draw_path_with_texture()
+        cv2.imshow("Path from Points", path_drawer.image)
 
-PathDrawer.draw_path_with_texture()
+    elif key == ord('u'):  # Undo the last point when 'u' is pressed
+        if len(path_drawer.selected_points) > 0:
+            path_drawer.selected_points.pop()
+            path_drawer.reset_image()
+            for point in path_drawer.selected_points:
+                cv2.circle(path_drawer.image, point, 5, path_drawer.points_color, -1)
+            cv2.imshow("Path from Points", path_drawer.image)
+            
+    elif key == ord('t'):  # Toggle the spline drawing when 't' is pressed
+        path_drawer.toggle_spline()
 
-cv2.imshow("Path from Points", PathDrawer.image)
-cv2.waitKey(0)
+    elif key == ord('q'):  # Exit the loop when 'q' is pressed
+        break
+
 cv2.destroyAllWindows()
