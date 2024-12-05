89241ec103bbf109f73e7e504ddcf9f35710f098
GOURNAY Gabriel <ggournay@banshee.enseeiht.fr>
2024-12-05 11:53:30 +0100

    pathing grossier et segmentation de masque

--- Interpolation/Images.py
+++ Interpolation/Images.py
@@ -1,18 +1,21 @@
 import cv2
-
+import numpy as np
 
 class ImageLoader():
-    def __init__(self, imagePath):
+    def __init__(self, n, imagePath="", image=None):
         self.imagePath = imagePath
-        self.image = cv2.imread(imagePath)
-        if self.image is None:
-            print("Image could not be loaded from : " + imagePath)
-        self.window_name = "my_window"
-        cv2.namedWindow("my_window")
+        if image is None:
+            self.image = cv2.imread(imagePath)
+            if self.image is None:
+                print("Image could not be loaded from : " + imagePath)
+        else:
+            self.image = image
+        self.window_name = "my_window" + str(n)
+        cv2.namedWindow(self.window_name)
         
-
     def show(self):
         cv2.imshow(self.window_name, self.image)
+        
 
     def show_and_wait(self):
         self.show()
@@ -22,6 +25,94 @@ class ImageLoader():
     def clear(self):
         cv2.destroyWindow(self.window_name)
 
+class ImagePathLoader(ImageLoader):
+    def __init__(self, n, bgPath="", bg=None, roadPath="", road=None):
+        super().__init__(n, bgPath, bg)
+        if road is None:
+            self.road = cv2.imread(roadPath)
+            if self.road is None:
+                print("Image could not be loaded from : " + roadPath)
+
+    def show_road(self):
+        cv2.imshow(self.window_name, self.road)
+        cv2.waitKey(0)
+
+    def resize_road(self, p):
+        h,w = self.image.shape[:2]
+        new_h = int(h * p) 
+        new_w = int(w * p)
+        self.road = cv2.resize(self.road, (new_h, new_w))
+
+    def resize_road_absolute(self, h, w):
+        self.road = cv2.resize(self.road, (h,w))
+
+
+    def get_draw_coordinates(self, x, y):
+        rh, rw = self.road.shape[:2]
+        bgh, bgw = self.image.shape[:2]
+
+        indices_x = np.array(range(rw))
+        draw_indices_x = np.array(range(int(x - rw/2), int(x + rw/2)))
+        indices_y = np.array(range(rh))
+        draw_indices_y = np.array(range(int(y - rh/2), int(y + rh/2)))
+        
+
+        
+        inside_x = np.logical_and(draw_indices_x >= 0, draw_indices_x < bgw)
+        inside_y = np.logical_and(draw_indices_y >= 0, draw_indices_y < bgh)
+        indices_x = indices_x[inside_x]
+        draw_indices_x = draw_indices_x[inside_x]
+        indices_y = indices_y[inside_y]
+        draw_indices_y = draw_indices_y[inside_y]
+
+        return indices_x, draw_indices_x, indices_y, draw_indices_y
+
+    def draw_road(self, x, y):
+        indices_x, draw_indices_x, indices_y, draw_indices_y = self.get_draw_coordinates(x,y)
+
+        for i in range(len(draw_indices_x)):
+            for j in range(len(draw_indices_y)):
+
+                    self.image[draw_indices_x[i], draw_indices_y[j], :] = self.road[indices_x[i], indices_y[j], :] / 255
+
+
+class ImageSegmenter(ImageLoader):
+    def __init__(self, n, maskPath="", mask=None):
+        super().__init__(n, imagePath=maskPath, image=mask)
+        
+        self.mask = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)
+        
+        self.num_labels, self.labels = cv2.connectedComponents(self.mask)
+
+    def get_mask_for_label(self, label):
+        return (self.labels == label).astype(int)
+
+    def draw_segmentation(self):
+        for label in range(self.num_labels):
+            component_mask = (self.labels == label).astype(int) 
+            n,m = component_mask.shape
+            to_draw = np.ones((n,m,3))
+            to_draw[:,:,0] = component_mask
+            to_draw[:,:,1] = component_mask
+            to_draw[:,:,2] = component_mask
+            cv2.imshow(self.window_name, to_draw)
+            cv2.waitKey(0)
+        
+
+def test_loader():
+    loader = ImageLoader(0, imagePath='Textures/texture3.png')
+    loader.show_and_wait()
+
+def test_path_loader():
+    loader = ImagePathLoader(0, bg=np.zeros((500,500,3)), roadPath='Textures/texture3.png')
+    loader.show_road()
+    loader.resize_road(0.1)
+    loader.draw_road(50, 50)
+    loader.show_and_wait()
+
+def test_segmenter():
+    segmenter = ImageSegmenter(0, maskPath="Textures/texture3_masque.png")
+    segmenter.draw_segmentation()
 
 if __name__ == '__main__':
-    ImageLoader('Textures/texture3.png').show()
+    test_segmenter()
--- Interpolation/Pathing.py
+++ Interpolation/Pathing.py
@@ -1,49 +1,112 @@
 import cv2
-from Images import ImageLoader
+from Images import ImageLoader, ImagePathLoader, ImageSegmenter
 from scipy.interpolate import splprep, splev
 import numpy as np
 
 
 class PathCreator():
-    def __init__(self, image_path):
-        self.loader = ImageLoader(image_path)
+    def __init__(self, loader):
+        self.loader = loader
         self.nb_of_points = 0
-        self.X = []
-        self.Y = []
+        self.X_input = []
+        self.Y_input = []
+        self.path = None
 
     def draw_point(self, x, y, color = (0,0,0)):
-        print("CENTER")
-        print(x,y)
         cv2.circle(self.loader.image, (x,y), 5, color, -1)
 
     def on_click(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
             self.draw_point(x, y, (255,0,0))
-            self.X.append(x)
-            self.Y.append(y)
+            self.X_input.append(x)
+            self.Y_input.append(y)
             self.nb_of_points += 1
 
     def capture_input(self, nb_of_points):
         cv2.setMouseCallback(self.loader.window_name, self.on_click)
+        self.nb_of_points = 0
+        self.X_input = []
+        self.Y_input = []
         while(self.nb_of_points < nb_of_points):
             self.loader.show()
             if cv2.waitKey(20) & 0xFF == 27:
                 break
 
-    def create_path(self, k):
-        tck, u = splprep(np.stack([self.X, self.Y]), s=0)
-        new_u = np.linspace(0, 1, 100)
-        path = splev(new_u, tck)
-        X = path[0]
-        Y = path[1]
-        print(X)
-        print(Y)
+    def create_path(self, k, n):
+        tck, u = splprep(np.stack([self.X_input, self.Y_input]), s=0, k=k)
+        new_u = np.linspace(0, 1, n)
+        self.path = splev(new_u, tck)
+        X = self.path[0]
+        Y = self.path[1]
         for i in range(len(X)):
             self.draw_point(round(X[i]), round(Y[i]))
-        self.loader.show_and_wait()
 
+    def draw_bounds(self, d):
+        height, width = self.loader.image.shape[:2]
+        y_d = range(height)
+        x_d = range(width)
+        x_d, y_d = np.meshgrid(x_d, y_d)
+        D = np.inf * np.ones_like(x_d)
+        x_p = self.path[0]
+        y_p = self.path[1]
+        for i in range(len(x_p)):
+            d_x = x_d - round(x_p[i])
+            d_y = y_d - round(y_p[i])
+            d_i = np.sqrt(np.square(d_x) + np.square(d_y))
+            D = np.minimum(D, d_i)
+        mask = (D <= d).astype(int)
+        to_draw = np.ones((height, width, 3))
+        to_draw[:, :, 0] *= mask
+        to_draw[:, :, 1] *= mask 
+        to_draw[:, :, 2] *= mask
+        self.path_mask = mask
+        self.path_sdf = D
+        ImageLoader(-1, image=to_draw).show_and_wait()
+
+
+class PathTexturer():
+    def __init__(self, path : PathCreator, loader : ImagePathLoader, segmenter : ImageSegmenter):
+        self.loader = loader
+        self.path = path
+        self.grid_shape = 0
+        self.grid_x = []
+        self.grid_y = []
+        self.texture_points = []
+
+    def sample_grid(self, n):
+        h,w = self.loader.image.shape[:2]
+        self.n = n
+        self.grid_x = np.linspace(0, w-1, n+1)
+        self.grid_y = np.linspace(0, h-1, n+1)
+        self.loader.resize_road_absolute(int(h/n), int(w/n))
+
+    def draw_on_grid(self):
+        self.texture_points = []
+        for x in self.grid_x:
+            for y in self.grid_y:
+                if self.path.path_mask[int(y),int(x)]:
+                    loader.draw_road(int(y), int(x))
+                    self.texture_points.append((x,y))
+
+    
+    def remove_outside(self):
+        # TO DO
+        pass
+
+          
 
 if __name__ == '__main__':
-    path = PathCreator('Textures/texture3.png')
-    path.capture_input(10)
-    path.create_path(3)
+    loader = ImagePathLoader(1, bg=np.zeros((500,500,3)), roadPath= "Textures/texture3.png")
+
+    path = PathCreator(loader=loader)
+    path.capture_input(5)
+    path.create_path(k=3, n=1000)
+    path.draw_bounds(d=20)
+
+    segmenter = ImageSegmenter(2, maskPath="Textures/texture3_masque.png")
+
+    texturer = PathTexturer(path, loader, segmenter)
+    texturer.sample_grid(20)
+    texturer.draw_on_grid()
+
+    loader.show_and_wait()
