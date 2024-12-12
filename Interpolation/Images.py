2255795ef39a77b18b603ec8944878e08afed503
GOURNAY Gabriel <ggournay@polaris.enseeiht.fr>
2024-12-12 09:48:37 +0100

    ca marche po

--- Interpolation/Images.py
+++ Interpolation/Images.py
@@ -52,11 +52,10 @@ class ImagePathLoader(ImageLoader):
         bgh, bgw = self.image.shape[:2]
 
         indices_x = np.array(range(rw))
-        draw_indices_x = np.array(range(int(x - rw/2), int(x + rw/2)))
+        draw_indices_x = np.array(range(x - int(rw/2), x + int(rw/2+1)))
         indices_y = np.array(range(rh))
-        draw_indices_y = np.array(range(int(y - rh/2), int(y + rh/2)))
+        draw_indices_y = np.array(range(y - int(rh/2), y + int(rh/2+1)))
         
-
         
         inside_x = np.logical_and(draw_indices_x >= 0, draw_indices_x < bgw)
         inside_y = np.logical_and(draw_indices_y >= 0, draw_indices_y < bgh)
@@ -82,21 +81,67 @@ class ImageSegmenter(ImageLoader):
         
         self.mask = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)
         
+        
+    def segment(self):
         self.num_labels, self.labels = cv2.connectedComponents(self.mask)
+
+    def resize_image(self, h, w):
+        self.image = cv2.resize(self.image, (h,w))
 
     def get_mask_for_label(self, label):
         return (self.labels == label).astype(int)
+    
+    def get_component(self, label):
+        component_mask = (self.labels == label).astype(int) 
+        return component_mask
+    
+    def get_components(self, labels):
+        components_mask = np.zeros(self.labels.shape)
+        for label in labels:
+            components_mask += self.get_component(label)
+        return components_mask
+    
+    def get_border(self):
+        n,m = self.labels.shape
+
+        up = set(list(self.labels[0, :]))
+        down = set(list(self.labels[n-1, :]))
+        left = set(list(self.labels[:, 0]))
+        right = set(list(self.labels[:, m-1]))
+
+        left.remove(0)
+        right.remove(0)
+        up.remove(0)
+        down.remove(0)
+
+        border = self.get_components(up.union(left.union(down.union(right))))
+        
+        return border, up, down, left, right
+    
+    
+    def get_neighbour(self, direction, label):
+        """
+        direction : 0=up, 1=left, 2=down, 3=right
+        """
+
 
     def draw_segmentation(self):
         for label in range(self.num_labels):
-            component_mask = (self.labels == label).astype(int) 
-            n,m = component_mask.shape
-            to_draw = np.ones((n,m,3))
-            to_draw[:,:,0] = component_mask
-            to_draw[:,:,1] = component_mask
-            to_draw[:,:,2] = component_mask
-            cv2.imshow(self.window_name, to_draw)
-            cv2.waitKey(0)
+            self.draw_segmentation_label(label)
+
+    def draw_segmentation_label(self,label):
+        component_mask = self.get_component(label)
+        self.draw_mask(component_mask)
+        
+
+    def draw_mask(self, mask):
+        n,m = mask.shape
+        to_draw = np.ones((n,m,3))
+        to_draw[:,:,0] = mask
+        to_draw[:,:,1] = mask
+        to_draw[:,:,2] = mask
+        cv2.imshow(self.window_name, to_draw)
+        cv2.waitKey(0)
         
 
 def test_loader():
@@ -112,7 +157,9 @@ def test_path_loader():
 
 def test_segmenter():
     segmenter = ImageSegmenter(0, maskPath="Textures/texture3_masque.png")
-    segmenter.draw_segmentation()
+    segmenter.segment()
+    b = segmenter.get_border()
+    #segmenter.draw_segmentation()
 
 if __name__ == '__main__':
     test_segmenter()
--- Interpolation/Pathing.py
+++ Interpolation/Pathing.py
@@ -42,6 +42,7 @@ class PathCreator():
             self.draw_point(round(X[i]), round(Y[i]))
 
     def draw_bounds(self, d):
+        d *= 2
         height, width = self.loader.image.shape[:2]
         y_d = range(height)
         x_d = range(width)
@@ -68,10 +69,13 @@ class PathTexturer():
     def __init__(self, path : PathCreator, loader : ImagePathLoader, segmenter : ImageSegmenter):
         self.loader = loader
         self.path = path
+        self.segmenter = segmenter
         self.grid_shape = 0
         self.grid_x = []
         self.grid_y = []
+        self.grid_mask = []
         self.texture_points = []
+        self.texture_points_coordinates = []
 
     def sample_grid(self, n):
         h,w = self.loader.image.shape[:2]
@@ -82,16 +86,69 @@ class PathTexturer():
 
     def draw_on_grid(self):
         self.texture_points = []
-        for x in self.grid_x:
-            for y in self.grid_y:
+        self.texture_points_coordinates = []
+        self.grid_mask = np.zeros((self.n + 1, self.n + 1)).astype(bool)
+        for i in range(self.n+1):
+            x = self.grid_x[i]
+            for j in range(self.n+1):
+                y = self.grid_y[j]
                 if self.path.path_mask[int(y),int(x)]:
                     loader.draw_road(int(y), int(x))
-                    self.texture_points.append((x,y))
+                    self.texture_points.append([int(x),int(y)])
+                    self.texture_points_coordinates.append((i,j))
+                    self.grid_mask[i,j] = True      
 
+
+    def get_border_info(self, i, j):
+        info = [False, False, False, False]
+        n,m = self.path.path_mask.shape
+        if j == 0:
+            info[0] = True
+        elif not (self.grid_mask[i,j-1]):
+            info[0] = True
+        if i == 0:
+            info[1] = True
+        elif not (self.grid_mask[i-1,j]):
+            info[1] = True
+        if i == m-1:
+            info[2] = True
+        elif not (self.grid_mask[i+1,j]):
+            info[2] = True
+        if j == n-1:
+            info[3] = True
+        elif not (self.grid_mask[i,j+1]):
+            info[3] = True
+        return info
     
-    def remove_outside(self):
-        # TO DO
-        pass
+    def remove_at(self, x, y, mask):
+        indices_x, draw_indices_x, indices_y, draw_indices_y = self.loader.get_draw_coordinates(x, y)
+        for i in range(len(draw_indices_x)):
+            for j in range(len(draw_indices_y)):
+                self.loader.image[draw_indices_y[j], draw_indices_x[i], :] *= (np.invert(mask[indices_y[j], indices_x[i]]))
+  
+    def remove_outside(self): 
+        rh,rw = self.loader.road.shape[:2]
+        self.segmenter.resize_image(rh, rw)
+        self.segmenter.segment()
+        border, up, down, left, right = self.segmenter.get_border()
+        for i in range(len(self.texture_points)):
+            t_p = self.texture_points[i]
+            t_p_c = self.texture_points_coordinates[i]
+            border_info = self.get_border_info(t_p_c[0], t_p_c[1])
+            border_labels = []
+            if border_info[0]:
+                border_labels += up
+            if border_info[1]:
+                border_labels += left
+            if border_info[2]:
+                border_labels += down
+            if border_info[3]:
+                border_labels += right
+            border_labels = set(border_labels)
+            mask = self.segmenter.get_components(border_labels)
+            self.remove_at(t_p[0], t_p[1], mask.astype(bool))
+                
+
 
           
 
@@ -99,8 +156,8 @@ if __name__ == '__main__':
     loader = ImagePathLoader(1, bg=np.zeros((500,500,3)), roadPath= "Textures/texture3.png")
 
     path = PathCreator(loader=loader)
-    path.capture_input(5)
-    path.create_path(k=3, n=1000)
+    path.capture_input(3)
+    path.create_path(k=1, n=20)
     path.draw_bounds(d=20)
 
     segmenter = ImageSegmenter(2, maskPath="Textures/texture3_masque.png")
@@ -108,5 +165,6 @@ if __name__ == '__main__':
     texturer = PathTexturer(path, loader, segmenter)
     texturer.sample_grid(20)
     texturer.draw_on_grid()
-
+    loader.show_and_wait()
+    texturer.remove_outside()
     loader.show_and_wait()
